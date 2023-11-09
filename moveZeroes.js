f06de48ff2aebd9e7962b7fbfcc92fded0336ce9
Raphael Santos <134030906+rphsantoss@users.noreply.github.com>
2023-11-09 10:37:19 -0300

    exe 2

---
+++ moveZeroes.js
@@ -0,0 +1,17 @@
+let moveZeroes = (nums) => {
+
+    let nums = [0,1,0,3,12] //-> [1,3,12,0,0]
+
+    let lastZero = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        
+        if(nums[i] !== 0) {    
+            const aux = nums[lastZero]
+            nums[lastZero] = nums[i]
+            nums[i] = aux 
+            lastZero++
+        }
+    }
+
+}
